37bdd8ea693d3e7d02a5709e489f856f40727dfc
Omri Fima <omrif@diagnosticrobotics.com>
2022-01-10 17:27:07 +0200

    on exit task

---
+++ tests/dsl/test_task_default_output.py
@@ -0,0 +1,23 @@
+import pytest
+import yaml
+
+from argo_workflow_tools import dsl, Workflow
+
+
+@dsl.Task(image="python:3.10")
+def say_hello(name: str):
+    message = f"hello {name}"
+    return message
+
+
+@dsl.DAG()
+def command_hello(name):
+    message = say_hello(name)
+    return message
+
+
+def test_diamond_params_run_independently():
+    workflow = Workflow(entrypoint=command_hello,name="command-hello",arguments={"name":"test"})
+    model = workflow.to_model()
+
+    assert model.spec.templates[0].outputs.parameters[0].value_from.default == ""
