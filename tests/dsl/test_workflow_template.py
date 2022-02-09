9a7f504e964796a77a702adf5f0c998537dad5b3
Ronbar <ronb@diagnosticrobotics.com>
2022-02-09 15:12:52 +0200

    Add workflow metadata labels and annotations

--- tests/dsl/test_workflow_template.py
+++ tests/dsl/test_workflow_template.py
@@ -2,15 +2,17 @@ import argo_workflow_tools.models.io.argoproj.workflow.v1alpha1 as argo
 from argo_workflow_tools import dsl, WorkflowTemplate
 
 
-def test_workflow_tempalte():
-    @dsl.Task(image="python:3.10")
-    def say_hello(name: str):
-        return f"hello {name}"
+@dsl.Task(image="python:3.10")
+def say_hello(name: str):
+    return f"hello {name}"
 
-    @dsl.DAG()
-    def command_hello(name):
-        return say_hello(name)
 
+@dsl.DAG()
+def command_hello(name):
+    return say_hello(name)
+
+
+def test_workflow_template():
     workflow = WorkflowTemplate(
         name="hello-world", entrypoint=command_hello, arguments={"name": "Brian"}
     )
@@ -18,15 +20,20 @@ def test_workflow_tempalte():
     assert model.kind == "WorkflowTemplate"
 
 
-def test_workflow_tempalte_arguments():
-    @dsl.Task(image="python:3.10")
-    def say_hello(name: str):
-        return f"hello {name}"
+def test_workflow_template_labels_and_annotations():
+    workflow = WorkflowTemplate(
+        name="hello-world", entrypoint=command_hello, arguments={"name": "Brian"},
+        labels={'key1': 'val1'}, workflow_labels={'key2': 'val2'},
+        annotations={'key1': 'val1'}, workflow_annotations={'key2': 'val2'},
+    )
+    model = workflow.to_model()
+    assert model.metadata.labels['key1'] == 'val1'
+    assert model.spec.workflow_metadata.labels['key2'] == 'val2'
+    assert model.metadata.annotations['key1'] == 'val1'
+    assert model.spec.workflow_metadata.annotations['key2'] == 'val2'
 
-    @dsl.DAG()
-    def command_hello(name):
-        return say_hello(name)
 
+def test_workflow_template_arguments():
     workflow = WorkflowTemplate(
         name="hello-world",
         entrypoint=command_hello,
