cb5173a13ac20b9d6b56c17d45a74a05cce49c49
8ball030 <35799987+8ball030@users.noreply.github.com>
2023-01-18 13:31:55 +0000

    Update workflow_status.py

--- argo_workflow_tools/workflow_status.py
+++ argo_workflow_tools/workflow_status.py
@@ -7,11 +7,15 @@ class WorkflowStatus(Enum):
     Failed = "Failed"
     Canceled = "Canceled"
     Suspended = "Suspended"
+    Created = "Created"
 
     @classmethod
     def value_of(cls, value):
+        """Return the enum value of the given value. if no value is found, return None."""
         for k, v in cls.__members__.items():
             if k == value:
                 return v
         else:
+            if value is None:
+                return WorkflowStatus.Created
             raise ValueError(f"'{cls.__name__}' enum not found for '{value}'")
